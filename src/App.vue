4bcdb2e7744fceb6215aa9c54ef89a4daa945524
techgoddess1 <57619820+techgoddess1@users.noreply.github.com>
2020-02-24 01:28:52 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator";
+export default {
+  name: "app",
+  components: {
+    Calculator
+  }
+};
+</script>
